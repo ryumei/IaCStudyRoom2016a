c0a3626b599d1c354ed440647950bb04d3ed9343
NAKAJIMA Takaaki <takaaki.nakajima.ya@d.mbsd.jp>
2016-06-17 14:40:37 +0900

    use template

--- docker/flask/app.py
+++ docker/flask/app.py
@@ -1,24 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 from os import environ
 import couchdb
+import logging
 
 app = Flask(__name__)
 
 COUCHDB_URL = environ['COUCHDB_URL'] if 'COUCHDB_URL' in environ \
                                      else 'http://localhost:5984/'
-server = couchdb.Server(COUCHDB_URL)
-
 DB_NAME = 'sample_db'
-if DB_NAME not in server:
-    server.create(DB_NAME)
+db_server = couchdb.Server(COUCHDB_URL)
+if DB_NAME not in db_server:
+    db_server.create(DB_NAME)
 
 @app.route("/")
 def hello():
-    body = ""
-    db = server[DB_NAME]
-    for id in db:
-        body += "document %s\n" % id
-    return "Hello World!\n" + body
+    return "Hello World!\n"
+
+@app.route("/list")
+def list():
+    db = db_server[DB_NAME]
+    return render_template("list_data.html", docs=[db[id] for id in db])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
